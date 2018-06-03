c23e94a0a687ae34bb576ea51305414b51c84db0
tomoyukionodera <tmykndr@gmail.com>
2018-06-04 00:58:16 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
+
+app.get(`/`, (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
